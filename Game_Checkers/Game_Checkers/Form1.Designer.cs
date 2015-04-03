54ac8a5aa6b2a9a35c83a08de32e310725dedfb7
Andriy <andriy.dudka@gmail.com>
2015-04-04 00:58:10 +0300

    Add project

---
+++ Game_Checkers/Game_Checkers/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Game_Checkers
+{
+    public partial class Game
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.Start = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(480, 480);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Mouse_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuStrip1.Size = new System.Drawing.Size(481, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Start,
+            this.toolStripSeparator1,
+            this.Exit});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(50, 20);
+            this.toolStripMenuItem1.Text = "Game";
+            // 
+            // Start
+            // 
+            this.Start.Name = "Start";
+            this.Start.ShortcutKeyDisplayString = " F1";
+            this.Start.ShortcutKeys = System.Windows.Forms.Keys.F1;
+            this.Start.Size = new System.Drawing.Size(154, 22);
+            this.Start.Text = "New Game";
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(151, 6);
+            // 
+            // Exit
+            // 
+            this.Exit.Name = "Exit";
+            this.Exit.ShortcutKeyDisplayString = " F2";
+            this.Exit.ShortcutKeys = System.Windows.Forms.Keys.F2;
+            this.Exit.Size = new System.Drawing.Size(154, 22);
+            this.Exit.Text = "Exit";
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(481, 507);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Game";
+            this.Text = "Checkers";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem Start;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem Exit;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Game_Checkers/Game_Checkers/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Game_Checkers
+{
+    public partial class Game : Form
+    {
+        
+        Graphics _graphics;
+        Bitmap _bitmap;
+        Pen _pen;
+        //шахова дошка
+        Rectangle[,] _rectangle;
+        //прямокутник жовтий
+        Rectangle _rec;
+        //закраска для чорнох квадратів
+        Brush _brushBlack;
+        //закраска для білих квадратів
+        Brush _brushWhite;
+        //закраска для червоних шашок
+        Brush _brushCheckers1;
+        //закраска для синіх шашок
+        Brush _brushCheckers2;
+        //координати ходів і прості змінні
+        int _x, _y, _xPrew, _yPrew, _iNext, _jNext, _i, _j,_iDeath,_jDeath;
+        //колір шашок в комірках
+        string[,] _color;
+        bool _one,_faith;
+        //конструктор гри
+        public Game()
+        {
+            _faith = false;
+            _one = true;
+            InitializeComponent();
+            pictureBox_Initialization();
+            Color_in_cell();
+            Chessboard();
+        }
+
+        public void pictureBox_Initialization()
+        {
+            _bitmap = new Bitmap(480,480);
+            //створюємо графіку для BitMap 
+            _graphics = Graphics.FromImage(_bitmap);
+            //колір ліній квадрата(жовтий)
+            _pen = new Pen(Color.Yellow, 3);
+            //закраска для чорної клітки
+            _brushBlack = new SolidBrush(Color.Black);
+            //закраска для білої клітки
+            _brushWhite = new SolidBrush(Color.White);
+            //закраска для червоної шашки
+            _brushCheckers1 = new SolidBrush(Color.Red);
+            //закраска для синьої шашки
+            _brushCheckers2 = new SolidBrush(Color.Blue);
+            //створення нового обєкта(дошки)
+            _rectangle = new Rectangle[8, 8];
+            //колір шашки в комірці
+            _color = new string[8, 8];
+        }
+
+        //метод який реалізує початковий колір шашок в комірках
+        public void Color_in_cell()
+        {
+            for (int i = 0; i < 8; i++)
+                for (int j = 0; j < 8; j++)
+                {
+                    if (i < 3 && (i + j) % 2 == 0) _color[i, j] = "Red";
+                    else
+                        if (i > 4 && (i + j) % 2 == 0) _color[i, j] = "Blue";
+                        else
+                            _color[i, j] = "White";
+                }
+        }
+
+        public void Chessboard()
+        {
+            _x = 0;
+            _y = 0;
+            //ініціалізація стану дошки
+            for (int i = 0; i < 8; i++)
+                for (int j = 0; j < 8; j++)
+                {
+                    if (j == 0)
+                    {
+                        _x = 0;
+                        _y = i * 60;
+                    }
+                    else _x += 60;
+                    _rectangle[i, j] = new Rectangle(_x, _y, 60, 60);                  
+                }
+        }
+        // запуск гри
+        public void Start_Click(object sender, EventArgs e)
+        {
+            Chessboard();
+            Color_in_cell();
+            //ініціалізація стану дошки і шашок в pictureBox1
+            Rectangle_initialization();
+            pictureBox1.Image = _bitmap;
+        }
+
+        //ініціалізація стану дошки і шашок
+        public void Rectangle_initialization()
+        {           
+            //ініціалізація стану дошки
+            for (int i = 0; i < 8; i++)
+                for (int j = 0; j < 8; j++)
+                {                  
+                    if ((i + j) % 2 == 0) _graphics.FillRectangle(_brushBlack, _rectangle[i, j]); 
+                    else 
+                        _graphics.FillRectangle(_brushWhite, _rectangle[i,j]);                
+                }
+            //ініціалізація стану шашок
+            for (int i = 0; i < 8; i++)
+                for (int j = 0; j < 8; j++)
+                    if (_color[i, j].CompareTo("Red") == 0) _graphics.FillEllipse(_brushCheckers1, _rectangle[i, j]); else
+            if (_color[i, j].CompareTo("Blue") == 0) _graphics.FillEllipse(_brushCheckers2, _rectangle[i, j]);
+        }
+        //вихід з гри
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        //Хід шашок, удар
+        private void Mouse_Click(object sender, MouseEventArgs e)
+        {
+            pictureBox1.Image = _bitmap;
+            //координати кліку мишки
+            _x = (int) e.X;
+            _y = (int) e.Y;
+            
+            _x = (_x / 60) * 60;
+            _y = (_y / 60) * 60;
+            //прямокутник жовтий(показує в яку клітку клікнули)
+            _rec = new Rectangle(_x,_y,58,58);
+            //координати комірки rectangle[i,j] попереднього кліку
+            _j = _xPrew/60;
+            _i = _yPrew/60;
+            //координати комірки rectangle[i_next,j_next] теперішнього кліку
+            _iNext = _y/60;
+            _jNext = _x/60;
+            //координати комірки rectangle[i_death,j_death] яку вдарили
+            _iDeath = 0;
+            _jDeath = 0;
+            if (_i - 2 == _iNext && _j - 2 == _jNext) { _iDeath = _i - 1; _jDeath = _j - 1; }
+            if (_i - 2 == _iNext && _j + 2 == _jNext) { _iDeath = _i - 1; _jDeath = _j + 1; }
+            if (_i + 2 == _iNext && _j - 2 == _jNext) { _iDeath = _i + 1; _jDeath = _j - 1; }
+            if (_i + 2 == _iNext && _j + 2 == _jNext) { _iDeath = _i + 1; _jDeath = _j + 1; }
+            
+
+            //прибираємо жовтий колір з попереднього ходу
+            if ((_i + _j) % 2 == 0) _graphics.FillRectangle(_brushBlack, _rectangle[_i, _j]); else _graphics.FillRectangle(_brushWhite, _rectangle[_i, _j]);
+            if (_color[_i, _j].CompareTo("Red") == 0) _graphics.FillEllipse(_brushCheckers1, _rectangle[_i, _j]);
+            else
+                if (_color[_i, _j].CompareTo("Blue") == 0) _graphics.FillEllipse(_brushCheckers2, _rectangle[_i, _j]);
+
+            //ставимо жовтий колір
+            if (_color[_iNext, _jNext].CompareTo("White") != 0) _graphics.DrawRectangle(_pen, _rec); else
+            
+            //хід шашки
+            if (_color[_i, _j].CompareTo("White") != 0 && Math.Abs(_jNext - _j) == 1 && Math.Abs(_iNext - _i) == 1
+                && _color[_iNext, _jNext].CompareTo("White") == 0 && ((_color[_i, _j].CompareTo("Red") == 0 && (_i - _iNext < 0)) 
+                || (_color[_i, _j].CompareTo("Blue") == 0 && (_i - _iNext > 0))) && ((_color[_i,_j].CompareTo("Red") == 0 && _one) || (_color[_i,_j].CompareTo("Blue") == 0 && !_one)) )
+            {
+                if (_one) _one = false; else _one = true;
+                _color[_iNext, _jNext] = _color[_i,_j];
+                if (_color[_i, _j].CompareTo("Red") == 0) _graphics.FillEllipse(_brushCheckers1, _rectangle[_iNext, _jNext]);
+                else
+                    _graphics.FillEllipse(_brushCheckers2, _rectangle[_iNext, _jNext]);
+                _color[_i, _j] = "White";
+                _graphics.FillRectangle(_brushBlack,_rectangle[_i,_j]);
+            } else
+                //удар противника
+                if (_color[_i, _j].CompareTo("White") != 0 && _color[_iNext, _jNext].CompareTo("White") == 0 
+                    && Math.Abs(_jNext - _j) == 2 && Math.Abs(_iNext - _i) == 2
+                    && _color[_i, _j].CompareTo(_color[_iDeath, _jDeath]) != 0 && _color[_iNext, _jNext].CompareTo(_color[_iDeath, _jDeath]) != 0
+                    && ((_color[_i, _j].CompareTo("Red") == 0 && _one) || (_color[_i, _j].CompareTo("Blue") == 0 && !_one)))
+            {
+                if (_one) _one = false; else _one = true;
+                _color[_iNext, _jNext] = _color[_i, _j];
+                if (_color[_i, _j].CompareTo("Red") == 0) _graphics.FillEllipse(_brushCheckers1, _rectangle[_iNext, _jNext]);
+                else
+                    _graphics.FillEllipse(_brushCheckers2, _rectangle[_iNext, _jNext]);
+                _color[_i, _j] = "White";
+                _color[_iDeath, _jDeath] = "White";
+                _graphics.FillRectangle(_brushBlack, _rectangle[_i, _j]);
+                _graphics.FillRectangle(_brushBlack, _rectangle[_iDeath, _jDeath]);
+            }
+             else
+                    //показуємо куди нажав ігрок
+                    _graphics.DrawRectangle(_pen, _rec);
+            //присвоєння поперднім координатам теперішні(кліка мишки)
+            _xPrew = _x;
+            _yPrew = _y;
+        }    
+    }
+}
